54a7841b334c88c2e27213ee2048eb54282dcc2a
mrshinobi <mrshinobidev@gmail.com>
2024-04-28 16:25:21 +0200

    add tests

---
+++ src/spark/udf/celsius_to_fahrenheit.py
@@ -0,0 +1,29 @@
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.functions import udf
+from pyspark.sql.types import DoubleType
+
+
+def convert_celsius_to_fahrenheit(celsius: float) -> float:
+    return (celsius * 9.0 / 5.0) + 32
+
+
+def main():
+    with SparkSession.builder.appName("UDFExample").getOrCreate() as spark:
+        df = create_data_frame(spark)
+
+        df_with_fahrenheit = create_df_with_fahreheit(df)
+        df_with_fahrenheit.show()
+
+
+def create_df_with_fahreheit(celsius_df: DataFrame) -> DataFrame:
+    fahrenheit_udf = udf(convert_celsius_to_fahrenheit, DoubleType())
+    df_with_fahrenheit = celsius_df.withColumn("Fahrenheit", fahrenheit_udf(celsius_df["Celsius"]))
+    return df_with_fahrenheit
+
+
+def create_data_frame(spark: SparkSession, df: DataFrame) -> DataFrame:
+    return spark.createDataFrame([(0,), (30,), (100,)], ["Celsius"])
+
+
+if __name__ == "__main__":
+    main()
